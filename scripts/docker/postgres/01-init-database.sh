7f889a5b57444667ff115608c7a6793997e281cf
Rafael G. <rafael.g.campi@gmail.com>
2025-01-26 18:53:14 -0300

    feat: comment docker compose

---
+++ scripts/docker/postgres/01-init-database.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+echo "Altering password for user: $POSTGRES_USER"
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "postgres" <<-EOSQL
+    ALTER USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_PASSWORD';
+EOSQL
+echo "Creating database: $POSTGRES_DB"
+
+export PGPASSWORD=$POSTGRES_PASSWORD
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE $POSTGRES_DB;
+    CREATE DATABASE test;
+    GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DB TO $POSTGRES_USER;
+EOSQL
+
+echo "Set PT_BR language"
+
+
