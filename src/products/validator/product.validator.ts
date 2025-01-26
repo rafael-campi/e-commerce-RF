54482d77d5148c3f488208ef53816b902435c81a
Rafael G. <rafael.g.campi@gmail.com>
2025-01-25 21:02:38 -0300

    feat: implements validator product

---
+++ src/products/validator/product.validator.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { ValidatorConstraint, ValidatorConstraintInterface } from 'class-validator';
+import { ValidationArguments } from 'class-validator';
+import { ProductsService } from '../products.service';
+
+@ValidatorConstraint({ async: true })
+@Injectable()
+export class ProductExists implements ValidatorConstraintInterface {
+  constructor(private readonly productService: ProductsService) {}
+
+  async validate(productId: number, args: ValidationArguments) {
+    const product = await this.productService.find(productId);
+    return !!product; 
+  }
+
+  defaultMessage(args: ValidationArguments) {
+    return `Product with ID ${args.value} does not exist`;
+  }
+}
